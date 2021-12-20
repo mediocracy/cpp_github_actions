a3669122cd7cbb1a74e0426af6172af77246ca9e
Matt <mediocratic@gmail.com>
2021-12-19 22:05:13 -0500

    add cmake project

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <adder.h>
 using namespace std;
 int main()
 {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << add(1, 2) << endl;
+    return 0;
 }
